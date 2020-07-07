d27f48b22f809ed8d8e9eeef955fd51553b0c339
vishal_146 <vishal@cognicept.com>
2020-07-08 02:07:05 +0530

    image based

--- index.js
+++ index.js
@@ -15,5 +15,10 @@ http.createServer(app).listen(8000);
 https.createServer(options, app).listen(8080);
 
 app.use(express.static(__dirname + '/www'));
+app.use(express.static('public'));
+
+app.get('/public/aviator.gltf', function(req, res){
+  res.sendFile(__dirname + '/public/aviator.gltf');
+});
 
 console.log('working on 8000 & 8080');
